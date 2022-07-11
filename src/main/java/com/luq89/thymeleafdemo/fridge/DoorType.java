f32ab2d85a0b184945f41af82ba0e2d3cbe14dfd
lukaszziobro89 <lukasz.ziobro.89@gmail.com>
2022-07-11 23:44:13 +0200

    added DoorType

---
+++ src/main/java/com/luq89/thymeleafdemo/fridge/DoorType.java
@@ -0,0 +1,19 @@
+package com.luq89.thymeleafdemo.fridge;
+
+public enum DoorType {
+
+    SIDE_BY_SIDE("Side by Side"),
+    TOP_FREEZER("Top freezer"),
+    BOTTOM_FREEZER("Bottom freezer"),
+    FRENCH_DOOR("French door");
+
+    private final String displayName;
+
+    DoorType(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+}
