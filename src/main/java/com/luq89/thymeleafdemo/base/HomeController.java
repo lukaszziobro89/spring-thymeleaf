6b6ed2139f5c73e3f61bf0537395419cb52e3a53
lukaszziobro89 <lukasz.ziobro.89@gmail.com>
2022-07-13 22:03:41 +0200

    home controller changes

---
+++ src/main/java/com/luq89/thymeleafdemo/base/HomeController.java
@@ -0,0 +1,13 @@
+package com.luq89.thymeleafdemo.base;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public String homePage(){
+        return "home";
+    }
+}
